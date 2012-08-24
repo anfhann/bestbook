a3a37b315af282459370965322b86799dd9e6de7
Bowen Turetzky <bturetzky@gmail.com>
2012-08-24 13:09:22 -0700

    Created a concordance, added p&p test data
    Created a simple concordance, prints out top 50 words used and words
    used exactly once.

--- simpleparse.py
+++ simpleparse.py
@@ -1,10 +1,35 @@
 import sys
 import string
+import operator
 
 filename = sys.argv[1]
 
 book = open(filename, 'r').read()
-words = book.translate(string.maketrans("",""), string.punctuation).split()
+for c in string.punctuation:
+	book = book.replace(c, " ")
+words = book.split()
 
-for word in words:
-	print word
+conc = {}
+
+for raw in words:
+	word = raw.lower()
+	if len(word) > 3:  #Throw out the short words
+		if conc.has_key(word):
+			conc[word] = conc[word]+1
+		else:
+			conc[word] = 1
+
+sorted_conc = sorted(conc.iteritems(), key=operator.itemgetter(1))
+
+print "\n50 Most common words:\n"
+for i in range(1, 50):
+	print sorted_conc[len(sorted_conc) - i]
+	
+print "\nUnique words:\n"
+count = 0
+for key in conc.keys():
+	if conc[key] == 1:
+		count+=1
+		print key
+
+print "\n" + str(count) + " words used exactly once\n"
